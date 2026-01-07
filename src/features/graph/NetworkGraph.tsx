4255d7bbdaf21fa06566ccbcf2785c5641aa867c
nonete <antonio.moreno@sjd.es>
2026-01-07 14:05:05 +0000

    style: remove unused custom scrollbar styles

--- src/features/graph/NetworkGraph.tsx
+++ src/features/graph/NetworkGraph.tsx
@@ -526,21 +526,6 @@ const NetworkGraph: React.FC = () => {
           }
         }}
       />
-
-      <style jsx>{`
-        .custom-scrollbar::-webkit-scrollbar {
-          width: 4px;
-        }
-        .custom-scrollbar::-webkit-scrollbar-track {
-          background: transparent;
-        }
-        .custom-scrollbar::-webkit-scrollbar-thumb {
-          background: rgba(255, 255, 255, 0.1);
-        }
-        .custom-scrollbar::-webkit-scrollbar-thumb:hover {
-          background: rgba(255, 255, 255, 0.2);
-        }
-      `}</style>
     </div>
   )
 }
